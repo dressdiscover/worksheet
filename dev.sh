98c4f144a42498b3247de41a09b0c029b60e09e4
Minor Gordon <minorg@server.fake>
2023-10-07 11:57:53 -0400

    update dev.sh

--- dev.sh
+++ dev.sh
@@ -7,7 +7,7 @@ cd `dirname "$(readlink -f "$0")"`
 THIS_DIR_PATH=$PWD
 CACHE_DIR_PATH=$THIS_DIR_PATH/.paradicms/cache
 DATA_DIR_PATH=$THIS_DIR_PATH/.paradicms/data
-PROJECTS_DIR_PATH=$PWD/../..
+PROJECTS_DIR_PATH=$PWD/..
 
 cd $PROJECTS_DIR_PATH/paradicms/lib/py/ssg
 
@@ -16,14 +16,14 @@ poetry run python $PROJECTS_DIR_PATH/paradicms/lib/py/etl/paradicms_etl/pipeline
   --paradicms-rdf-file-path $DATA_DIR_PATH/costume_core_ontology.trig \
   "$@"
 
-poetry run $PROJECTS_DIR_PATH/directory-etl-action/action.py \
+poetry run $PROJECTS_DIR_PATH/paradicms-directory-etl-action/action.py \
   --cache-directory-path $CACHE_DIR_PATH \
   --input-directory-path $THIS_DIR_PATH \
   --loaded-data-directory-path $DATA_DIR_PATH \
   --pipeline-id app_configuration
 
-poetry run $PROJECTS_DIR_PATH/ssg-action/action.py \
+poetry run $PROJECTS_DIR_PATH/paradicms-ssg-action/action.py \
   --cache-directory-path $CACHE_DIR_PATH \
   --data-paths $DATA_DIR_PATH \
-  --dev \
+  --next-command dev \
   --pipeline-id app
